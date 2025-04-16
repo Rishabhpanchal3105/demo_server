47076228a64828bbb7219349a8dc3532b0ff1c12
Rishabh <rishabh.softcolon@gmail.com>
2025-04-16 16:39:00 +0530

    handling the api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from datetime import date , time
+from datetime import date, time
 import logging
 import uvicorn
 logger = logging.getLogger(__name__)
@@ -14,31 +14,37 @@ app.add_middleware(
     allow_origins=['*']
 )
 
+
 class QueryModel(BaseModel):
     name: str
     date: date
     time: time
 
+
 class BookAppointmentRequest(BaseModel):
     query: QueryModel
+
 
 @app.post('/book_appointment')
 async def book_appointment(appointment: BookAppointmentRequest):
     # In a real app, you would save this to a database
-    
-    logger.info({"message": "Appointment is booked", "appointment_details": {
-        "name": appointment.query.name,
-        "date": appointment.query.date,
-        "time": appointment.query.time
-    }})
-    return {
-        "message": "Appointment is booked",
-        "appointment_details": {
+    try:
+        logger.info({"message": "Appointment is booked", "appointment_details": {
             "name": appointment.query.name,
             "date": appointment.query.date,
             "time": appointment.query.time
+        }})
+        return {
+            "message": "Appointment is booked",
+            "appointment_details": {
+                "name": appointment.query.name,
+                "date": appointment.query.date,
+                "time": appointment.query.time
+            }
         }
-    }
+    except Exception as e:
+        logger.info(str(e))
+        raise e
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=5000)
